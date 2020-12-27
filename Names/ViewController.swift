95ef4fcdef9b205f5f79d3fe226738319f17b779
Nikita Petrenkov <nikita.petrenkov@rtlabs.ru>
2020-12-27 21:36:59 +0400

    Initial Commit

---
+++ Names/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Names
+//
+//  Created by Nikita Petrenkov on 27/12/2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
